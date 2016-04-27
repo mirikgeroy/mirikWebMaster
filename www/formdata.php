70fa190b4c60d6c5389162e26324b89a1e0d5f91
mirikgeroy <tkamir@gmail.com>
2016-04-27 13:12:11 +0300

    my first commit

---
+++ www/formdata.php
@@ -0,0 +1,18 @@
+
+<?php
+
+echo $_GET['sleep'];
+/**
+echo 'ddd';
+
+$result = mail("test@test.ru","анкета дрочуна","тело");
+
+if ($result) {
+	echo "<p>лист відправлено</p>";
+}
+else {
+	echo "<p>лист не відправлено</p>";
+	} 
+	**/
+
+?>
